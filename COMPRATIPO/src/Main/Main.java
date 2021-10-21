c1d4a31c645792aef431f356131225898c4d1a01
Arthur Cáceres Melo <87779496+meloarthur@users.noreply.github.com>
2021-10-21 10:48:36 -0300

    Add files via upload

---
+++ COMPRATIPO/src/Main/Main.java
@@ -0,0 +1,15 @@
+package Main;
+
+import View.Menu;
+
+public class Main {
+
+    //Abrir menu
+    public static void main(String[] args) {
+        
+        Menu menu = new Menu();
+        menu.setVisible(true);
+
+        }
+        
+    }
